0bb6eeb818d8678b902c4f1f9d99a0589331481c
NkompChristian <christian.nkomp@edu.itspiemonte.it>
2018-05-07 11:21:05 +0200

    fzgfxhg

--- ProjectsTool/Controllers/HomeController.cs
+++ ProjectsTool/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace ProjectsTool.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         public ActionResult Index()
@@ -13,19 +14,13 @@ namespace ProjectsTool.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Resources()
         {
-            ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+     
 
     }
 }
