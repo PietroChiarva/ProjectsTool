74c51143af8b7065f09582af3ff0f504d3c78924
NkompChristian <christian.nkomp@edu.itspiemonte.it>
2018-05-07 17:23:05 +0200

    work1

---
+++ ProjectsTool/Models/ResourceList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectsTool.Models
+{
+    public class ResourceList : Person
+    {
+        public ResourceList()
+        {
+            ResultList = new List<Person>();
+        }
+        public List<Person> ResultList { get; set; }
+
+    }
+}
