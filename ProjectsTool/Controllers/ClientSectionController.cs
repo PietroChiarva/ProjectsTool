97d0acaac299f2204d0a5feba2d9efa9db04624b
NkompChristian <christian.nkomp@edu.itspiemonte.it>
2018-05-09 17:21:25 +0200

    Resources Control

--- ProjectsTool/Controllers/ClientSectionController.cs
+++ ProjectsTool/Controllers/ClientSectionController.cs
@@ -58,5 +58,86 @@ namespace ProjectsTool.Controllers
                 return Json(new { messaggio = $"Dati mancanti o non validi", flag = false });
             }
         }
+
+        public ActionResult ClientProject(int IDClient)
+        {
+           
+            List<Project> projectclient = null;
+           
+
+            using (ProjectToolsEntities db = new ProjectToolsEntities())
+            {
+                projectclient = db.Project.Where(l => l.IDClient == IDClient).ToList();
+               
+
+                  
+                }
+          
+            return PartialView(projectclient);
+        }
+
+        public ActionResult EditClient(int IDClient)
+        {
+            Client client = null;
+            using (ProjectToolsEntities db = new ProjectToolsEntities())
+            {
+                client = db.Client.Where(l => l.IDClient == IDClient).FirstOrDefault();
+
+            }
+            return PartialView(client);
+            
+            
+        }
+
+        public ActionResult DoEditClient(Client data)
+        {
+            Client client = null;
+            using (ProjectToolsEntities db = new ProjectToolsEntities())
+            {
+                client = db.Client.Where(l => l.IDClient == data.IDClient).FirstOrDefault();
+                client.Name = data.Name;
+                client.Society = data.Society;
+                client.NumberPhone = data.NumberPhone;
+                client.Email = data.Email;
+                client.PartitaIva = data.PartitaIva;
+                db.SaveChanges();
+
+            }
+            return RedirectToAction("ClientSection");
+
+        }
+
+
+
+        public ActionResult DeleteClient(int IDClient)
+        {
+            Client client = null;
+            List<ActiveProject> activeProject = new List<ActiveProject>();
+            using (ProjectToolsEntities db = new ProjectToolsEntities())
+            {
+                client = db.Client.Where(l => l.IDClient == IDClient).FirstOrDefault();
+                activeProject = db.ActiveProject.Where(l => l.IDProject == client.IDClient).ToList();
+                if (activeProject.Count == 0)
+                {
+                    TempData["msg"] = "<script>alert('This client have got some project active!');</script>";
+                }
+            }
+            return PartialView(client);
+        }
+
+        public ActionResult DoDeleteClient(int IDClient)
+        {
+            Client client = null;
+
+            using (ProjectToolsEntities db = new ProjectToolsEntities())
+            {
+                client = db.Client.Where(l => l.IDClient == IDClient).FirstOrDefault();
+
+                db.Client.Remove(client);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
     }
 }
--- ProjectsTool/Controllers/ResourcesController.cs
+++ ProjectsTool/Controllers/ResourcesController.cs
@@ -10,8 +10,8 @@ namespace ProjectsTool.Controllers
 {
     public class ResourcesController : Controller
     {
-        
-      
+
+
         public ActionResult Resources()
         {
             using (ProjectToolsEntities db = new ProjectToolsEntities())
@@ -32,7 +32,7 @@ namespace ProjectsTool.Controllers
             using (ProjectToolsEntities db = new ProjectToolsEntities())
             {
                 activeProjects = db.ActiveProject.Where(l => l.IDPerson == IDPerson).ToList();
-                foreach(ActiveProject a in activeProjects)
+                foreach (ActiveProject a in activeProjects)
                 {
                     projects = db.ActiveProject.Include(m => m.Project).Where(l => l.IDProject == a.IDProject).Select(l => new SingleProjectModel()
                     {
@@ -47,13 +47,43 @@ namespace ProjectsTool.Controllers
                     proj.Add(projects[0]);
                 }
             }
-                return PartialView(proj);
+            return PartialView(proj);
         }
 
         public ActionResult SeeProject(int IDPerson)
         {
 
             return PartialView();
+        }
+
+        public ActionResult ProjectStoryResource(int IDPerson)
+        {
+            List<SingleProjectModel> projects = new List<SingleProjectModel>();
+            List<ActiveProject> activeProjects = new List<ActiveProject>();
+            List<SingleProjectModel> proj = new List<SingleProjectModel>();
+
+            using (ProjectToolsEntities db = new ProjectToolsEntities())
+            {
+                activeProjects = db.ActiveProject.Where(l => l.IDPerson == IDPerson).ToList();
+                foreach (ActiveProject a in activeProjects)
+                {
+                    projects = db.ActiveProject.Include(m => m.Project).Where(l=> l.IDPerson == IDPerson && l.EndActiveDate < DateTime.Now).Select(l => new SingleProjectModel()
+                    {
+                        ProjectName = l.Project.ProjectName,
+                        StartDate = l.Project.StartDate,
+                        EndDate = l.Project.EndDate,
+                        IsFinish = l.Project.IsFinish,
+                        Percentage = l.Percentage,
+
+                    }).ToList();
+                    
+
+
+
+                }
+
+            }
+            return PartialView(projects);
         }
     }
 }
