45395c391869bf522cd2314adb5b4194a2100451
PietroChiarva <pietro.chiarva@edu.itspiemonte.it>
2018-05-11 15:06:09 +0200

    roba

---
+++ ProjectsTool/Models/data.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectsTool.Models
+{
+    public class data
+    {
+        public int IDPerson { get; set; }
+        public int IDProject { get; set; }
+    }
+}
